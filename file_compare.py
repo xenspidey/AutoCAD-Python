6ae24fd59a8626b08dc11d03dca7055bc3739908
xenspidey <xenspidey@gmail.com>
2017-02-19 21:35:20 -0500

    listing open drawings

--- file_compare.py
+++ file_compare.py
@@ -1,11 +1,20 @@
+"""python tools for autocad
+"""
 from pyautocad import Autocad
 
+
 def main():
+    """main program function"""
     cad = Autocad(create_if_not_exists = True)
     open_docs = cad.app.documents
+    doc_list = []
     for open_doc in open_docs:
         print(open_doc.path + "\\" + open_doc.name)
-        
+        doc_list.append(open_doc)
+    if open_docs.__sizeof__ != 2:
+        print("only two drawings can be comparred")
+        print(len(doc_list))
+
 
 if __name__ == '__main__':
     main()
