517c504d7249f024c5454525abe51dcf5786d428
bcalendine <bcalendine>
2017-02-20 17:03:23 -0500

    Added gui elements

---
+++ autocad.py
@@ -0,0 +1,50 @@
+"""Autocad testing
+"""
+from pyautocad import Autocad, APoint
+from tkinter import *
+
+
+def compare(cad):
+    """main program function"""
+    open_docs = cad.app.documents
+    doc_list = []
+    for open_doc in open_docs:
+        print(open_doc.path + "\\" + open_doc.name)
+        doc_list.append(open_doc)
+    if len(doc_list) != 2:
+        print("only two drawings can be comparred")
+
+
+def load_att(cad):
+    """reload attribute
+    reload the attribute block located in the xref folder
+    """
+    is_att = False
+    for block in cad.iter_objects_fast("block"):
+        if block.name == "attribute" or block.name == "Attribute":
+            block.delete()
+            is_att = True
+            break
+    if not is_att:
+        cad_path = cad.ActiveDocument.path()
+        print(cad_path)
+        for layouts in cad.iter_layouts(doc=None, skip_model=True):
+            print(layouts.name)
+            cad.doc.activelayout = layouts
+            cad.doc.paperspace.InsertBlock(
+                APoint(0, 0), cad_path + "\\xref\\Attribute.dwg", 1, 1, 1, 0)
+
+
+def main():
+    '''gui portion'''
+    cad = Autocad(create_if_not_exists=True)
+    root = Tk()
+    root.geometry('500x100+1000+500')
+    button_compare = Button(root, text='Compare', command=lambda: compare(cad))
+    button_loadatt = Button(root, text='Load_att', command=lambda: load_att(cad))
+    button_compare.pack(pady=10, padx=10)
+    button_loadatt.pack(pady=10, padx=10)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
--- file_compare.py
+++ file_compare.py
@@ -1,11 +1,11 @@
 """python tools for autocad
 """
 from pyautocad import Autocad
-
+from tkinter import *
 
 def main():
     """main program function"""
-    cad = Autocad(create_if_not_exists = True)
+    cad = Autocad(create_if_not_exists=True)
     open_docs = cad.app.documents
     doc_list = []
     for open_doc in open_docs:
@@ -16,5 +16,14 @@ def main():
         print(len(doc_list))
 
 
+def gui():
+    '''gui portion'''
+    root = Tk()
+    root.geometry('500x100+1000+500')
+    button = Button(root, text='Compare', command=main)
+    button.pack(pady=20, padx=20)
+    root.mainloop()
+
+
 if __name__ == '__main__':
-    main()
+    gui()
